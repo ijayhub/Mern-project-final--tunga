df8aa2c9333643476abd070f1ff6c613ae74c32f
Ijeoma Igboagu <ijeonyi@gmail.com>
2023-10-28 12:57:14 +0100

    update
    update: Improving the performance of the application route at app.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,33 @@
+import { lazy,Suspense } from "react"
 import Nav from "./components/Nav"
-import AddMovies from "./pages/AddMovies"
-import Home from "./pages/Home"
-import MovieDetails from "./pages/MovieDetails"
 import {Routes, Route} from "react-router-dom"
-import MoviesList from "./pages/MoviesList"
-import MovieDelete from './pages/MovieDelete';
 
+
+// To improve performance of the application
+const Home = lazy(() => import ("./pages/Home"))
+const MovieDetails = lazy(() =>import("./pages/MovieDetails"));
+const MovieDelete = lazy(() => import("./pages/MovieDelete"));
+const MoviesList = lazy(() => import("./pages/MoviesList"));
+const AddMovies = lazy(() =>import ("./pages/AddMovies"));
 
 const App = () => {
   return (
 		<div>
 			<Nav />
-			<Routes>
-				<Route path='/' element={<Home />} />
-				<Route path='/movies/details/:imdbID' element={<MovieDetails />} />
-				<Route path='/movies/delete/:id' element={<MovieDelete />} />
-				<Route path='/list' element={<MoviesList />} />
-				<Route path='/add' element={<AddMovies />} />
-			</Routes>
+			<Suspense
+				fallback={
+					<h1 className='text-2xl lg:text-4xl font-mono font-bold text-center mt-8'>
+						Loading...
+					</h1>
+				}>
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route path='/movies/details/:imdbID' element={<MovieDetails />} />
+					<Route path='/movies/delete/:id' element={<MovieDelete />} />
+					<Route path='/list' element={<MoviesList />} />
+					<Route path='/add' element={<AddMovies />} />
+				</Routes>
+			</Suspense>
 		</div>
 	);
 }
